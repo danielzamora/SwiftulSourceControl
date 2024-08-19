9f5652290c06e7a811b1e7b774b42e8657655b1b
Daniel Zamora <dzamora7@gmail.com>
2024-08-19 01:15:02 -0500

    Initial Commit

---
+++ SwiftulSourceControl/SwiftulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulSourceControlApp.swift
+//  SwiftulSourceControl
+//
+//  Created by Daniel Zamora on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
